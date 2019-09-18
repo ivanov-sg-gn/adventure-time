e7c62bbf52244a3f72aea7ebe5d14c9ff9b91e4b
Sergey <53167209+ivanov-sg-gn@users.noreply.github.com>
2019-09-19 01:33:40 +0300

    Update

--- php-overload.php
+++ php-overload.php
@@ -1,49 +1,54 @@
 <?php
 
-class overload{
-	public function get(){
-		if(func_num_args() != 1){
-			throw new Exception('Method expects 1 parameter');
-		}
-		
-		$arg = func_get_arg(0);
-		
-		$typeParam = $this->getType($arg);
+class overload
+{
+    public function get()
+    {
+        if (func_num_args() != 1) {
+            throw new Exception('Method expects 1 parameter');
+        }
 
-		switch($typeParam){
-			case 'integer':
-				$this->getById($arg);
-				break;
-			case 'array':
-				$this->getByFilter($arg);
-				break;
-		}
-	}	
-	
-	private function getType($param){
-		$type = gettype($param);
-		
-		if($type == 'string'){
-			if(DateTime::createFromFormat('Y-m-d H:i:s', $param) !== FALSE){
-				return 'date';
-			}
-		}
-		
-		return gettype($param);
-	}
-	
-	public function getById(int $id) : array{
-		// ...
-		
-		return [];
-	}
-	
-	public function getByFilter(array $arr) : array{
-		// ...
-		
-		return [];
-	}
-	
+        $arg = func_get_arg(0);
+
+        $typeParam = $this->getType($arg);
+
+        switch ($typeParam) {
+            case 'integer':
+                $this->getById($arg);
+                break;
+            case 'array':
+                $this->getByFilter($arg);
+                break;
+        }
+    }
+
+    private function getType($param)
+    {
+        $type = gettype($param);
+
+        if ($type == 'string') {
+            if (DateTime::createFromFormat('Y-m-d H:i:s', $param) !== FALSE) {
+                return 'date';
+            }
+        }
+
+        return gettype($param);
+    }
+
+    public function getById(int $id): array
+    {
+        // ...
+
+        return [];
+    }
+
+    public function getByFilter(array $arr): array
+    {
+        // ...
+
+        return [];
+    }
+
 }
 
 $overload = new overload;
